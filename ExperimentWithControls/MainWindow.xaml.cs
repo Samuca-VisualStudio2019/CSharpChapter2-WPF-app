0dd4b19e15efb9bf68690f5595c25cde272d1e56
Samuel Santos Barreto Pereira <samucateclas@outlook.com>
2021-02-24 22:49:28 -0300

    Page 79 finished

--- ExperimentWithControls/MainWindow.xaml.cs
+++ ExperimentWithControls/MainWindow.xaml.cs
@@ -24,5 +24,16 @@ namespace ExperimentWithControls
         {
             InitializeComponent();
         }
+
+    
+        private void numberTextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            number.Text = numberTextBox.Text;
+        }
+
+        private void numberTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !int.TryParse(e.Text, out int result);
+        }
     }
 }
